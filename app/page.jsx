b676867b119836bbed7bd7170b03255e5b15d108
MicromeX <hello@thecreative.company>
2026-06-23 05:36:26 +0800

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -112,7 +112,7 @@ export default function Home() {
           <div className="hero-right">
             <div className="vision-video parallax-object" data-parallax="0.12">
               <iframe
-                src="https://www.youtube.com/embed/8HDjaAV_12s?autoplay=1&mute=1&playsinline=1&rel=0&modestbranding=1&loop=1&playlist=8HDjaAV_12s"
+                src="https://www.youtube.com/embed/R_FuIcXQSow?autoplay=1&mute=1&playsinline=1&rel=0&modestbranding=1&loop=1&playlist=R_FuIcXQSow"
                 title="Planting the Bay vision video"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 allowFullScreen
